760b3eb2fea874640479afacc44ad54341a2fa70
tor-ato <toratokitahara@gmail.com>
2025-03-09 17:54:14 +0900

    [cpp01] ex02: done

---
+++ cpp-module-01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	const std::string brain = "HI THIS IS BRAIN";
+
+	const std::string *stringPTR = &brain;
+	const std::string &stringREF =brain;
+
+	std::cout << "Address of brain: " << &brain << std::endl;
+	std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Valeu of brain: " << brain << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
